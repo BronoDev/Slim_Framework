5892dc7581b9e8b0082ee7292f67d528b691c84e
Brono <brunobrunelli1995@gmail.com>
2023-09-06 11:48:22 -0300

    :sparkles:	feat: Rotas e subrotas

--- Public/index.php
+++ Public/index.php
@@ -1,17 +1,51 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-//use Slim\Factory\AppFactory;
 
-require  'A:\Projetos\Slim_Framework\vendor\autoload.php';
+require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
+$app->get('/postagens2', function () {
+    echo "Listagem de postagens";
 });
+
+$app->get('/usuarios[/{id}]', function ($request, $response) {
+    $id = $request->getAttribute('id');
+    echo "Listagem de usuarios ou ID: " . $id;
+});
+
+$app->get('/postagens[/{ano}[/{mes}]]', function ($request, $response) {
+    $ano = $request->getAttribute('ano');
+    $mes = $request->getAttribute('mes');
+    echo "Listagem de postagens " . " Ano: " . $ano . " Mes: " . $mes;
+});
+
+$app->get('/lista/{itens:.*}', function ($request, $response) {
+    $itens = $request->getAttribute('itens');
+
+    var_dump(explode("/", $itens));
+
+});
+
+$app->get('/blog/postagens/{id}', function ($request, $response) {
+    echo "Listar postagem para um ID";
+})->setName("blog");
+
+$app->get('/meusite', function ($request, $response) {
+    $retorno = $this->get("router")->pathFor("blog", ["id" => "5"]);
+    echo $retorno;
+});
+
+
+$app->group('/v1', function () {
+    $this->get('/usuarios', function () {
+        echo "Listagem de usuarios";
+    });
+
+    $this->get('/postagens', function () {
+        echo "Listagem de postagens";
+    });
+});
+
+
 
 $app->run();
